0fc20ca8a3d3c16b443126d5c12f8e67699574db
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-03 23:24:36 +0600

    [Done] Separate success & error logs and cutomize the log

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import logger from './shared/logger'
+import { errorLogger, logger } from './shared/logger'
 
 async function bootstrap() {
   try {
@@ -14,7 +14,7 @@ async function bootstrap() {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    logger.error('Failed to connect database❌', err)
+    errorLogger.error('Failed to connect database❌', err)
   }
 }
 
