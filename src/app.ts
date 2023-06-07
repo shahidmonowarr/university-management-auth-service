165b3f5975b4a393db234d97fae569cccb8acedd
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-07 18:31:56 +0600

    [Done] Academic Semester Model Complete & Code Optimizations

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { UserRoutes } from './app/modules/users/user.route';
+import routes from './app/routes';
 const app: Application = express();
 
 app.use(cors());
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users/', UserRoutes);
+// routes
+app.use('/api/v1/', routes);
 
 // testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
