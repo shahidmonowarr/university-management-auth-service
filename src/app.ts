14e281eee21ba48b1b8180620728ec1c5972697f
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-07 10:01:41 +0600

    prettierrc semicolon true

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 // express server setup
-import cors from 'cors'
-import express, { Application } from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.route'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/users/user.route';
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 // Parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/users/', UserRoutes);
 
 // testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
@@ -19,6 +19,6 @@ app.use('/api/v1/users/', UserRoutes)
 // })
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,48 @@
 // database connection related code
-import { Server } from 'http'
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorLogger, logger } from './shared/logger';
 
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
 async function bootstrap() {
-  let server: Server
+  let server: Server;
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('Connected to database🛢 successfully')
+    await mongoose.connect(config.database_url as string);
+    logger.info('Connected to database🛢 successfully');
 
     // app listening
     server = app.listen(config.port, () => {
-      logger.info(`Application listening on port ${config.port}`)
-    })
+      logger.info(`Application listening on port ${config.port}`);
+    });
   } catch (err) {
-    errorLogger.error('Failed to connect database❌', err)
+    errorLogger.error('Failed to connect database❌', err);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error)
-        process.exit(1)
-      })
+        errorLogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-bootstrap()
+bootstrap();
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is Received')
+  logger.info('SIGTERM is Received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
