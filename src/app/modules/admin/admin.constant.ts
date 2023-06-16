ae9f393cc50829ee1bf9429c496af1a2f9da456b
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-16 10:08:57 +0600

    [Done] Admin Created with Implemented Transaction and Rollback

---
+++ src/app/modules/admin/admin.constant.ts
@@ -0,0 +1,23 @@
+export const gender = ['male', 'female'];
+
+export const bloodGroup = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+
+export const adminSearchableFields = [
+  'id',
+  'email',
+  'contactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
+  'designation',
+];
+
+export const adminFilterableFields = [
+  'searchTerm',
+  'id',
+  'bloodGroup',
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'designation',
+];
