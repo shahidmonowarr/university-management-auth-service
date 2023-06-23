afb374ccfc32f24819b88cacf717494738d6c0b0
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-23 16:31:34 +0600

    [Done] Create accessToken and refreshToken

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
+import { JwtHelpers } from '../../../helpers/jwtHelpers';
 import { User } from '../users/user.model';
-import { ILoginUser } from './auth.interface';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
 
-const loginUser = async (payload: ILoginUser) => {
+const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
   // create a new instance of the User model
   //const user = new User();
@@ -18,12 +21,37 @@ const loginUser = async (payload: ILoginUser) => {
 
   if (
     isUserExist.password &&
-    !(await User.isPasswordMatched(password, isUserExist?.password))
+    !(await User.isPasswordMatched(password, isUserExist.password))
   ) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password Does Not Match');
   }
 
   // create access token and refresh token
+  const { id: userId, role, needsPasswordChange } = isUserExist;
+
+  const accessToken = JwtHelpers.createToken(
+    {
+      userId,
+      role,
+    },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = JwtHelpers.createToken(
+    {
+      userId,
+      role,
+    },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needsPasswordChange,
+  };
 };
 
 export const AuthService = {
