3782330abbfe23397bea735142e3ae9387b88d81
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-23 23:49:48 +0600

    [Done] Authentication, authorization & verify token using auth middleware

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -23,6 +23,8 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+  // console.log(req.headers.authorization);
+  // console.log(req.user);
   const filters = pick(req.query, academicFacultyFilterableField);
   const paginationOptions = pick(req.query, paginationFields);
 
