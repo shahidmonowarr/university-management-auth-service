38e836321f87f644c14923413db23392c2507e96
shahidmonowarr <shahidmonowar21@gmail.com>
2023-06-23 21:38:09 +0600

    [Done] Refresh token handler Created with refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 // express server setup
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
@@ -7,7 +8,7 @@ import routes from './app/routes';
 const app: Application = express();
 
 app.use(cors());
-
+app.use(cookieParser());
 // Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -4,7 +4,11 @@ import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
 import { JwtHelpers } from '../../../helpers/jwtHelpers';
 import { User } from '../users/user.model';
-import { ILoginUser, ILoginUserResponse } from './auth.interface';
+import {
+  ILoginUser,
+  ILoginUserResponse,
+  IRefreshTokenResponse,
+} from './auth.interface';
 
 const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
@@ -54,6 +58,43 @@ const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   };
 };
 
+const refreshToken = async (token: string): Promise<IRefreshTokenResponse> => {
+  // verify refresh token
+  let verifiedToken = null;
+  try {
+    verifiedToken = JwtHelpers.verifyToken(
+      token,
+      config.jwt.refresh_secret as Secret
+    );
+
+    //
+  } catch (error) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid Refresh Token');
+  }
+
+  const { userId } = verifiedToken;
+
+  // checking deleted user's refresh token
+  const isUserExist = await User.isUserExist(userId);
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User Does Not Exist');
+  }
+  // Generate new token
+  const newAccessToken = JwtHelpers.createToken(
+    {
+      id: isUserExist.id,
+      role: isUserExist.role,
+    },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  return {
+    accessToken: newAccessToken,
+  };
+};
+
 export const AuthService = {
   loginUser,
+  refreshToken,
 };
